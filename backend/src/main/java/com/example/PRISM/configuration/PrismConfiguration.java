4f37865b205da43e241118dc83ce3afa61f63515
VinithS123 <vinithselvaraj123@gmail.com>
2026-05-09 22:43:36 +0530

    added cors

--- backend/src/main/java/com/example/PRISM/configuration/PrismConfiguration.java
+++ backend/src/main/java/com/example/PRISM/configuration/PrismConfiguration.java
@@ -1,4 +1,26 @@
 package com.example.PRISM.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class PrismConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://industrial-realtime-monitoring-syst.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+
 }
